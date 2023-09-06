034a3d83e0af9510fa215b0ced36166c14a0b707
asher gray <asher@themakerspace.co.za>
2023-09-06 14:16:31 +0200

    added themes
    themes

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 window.addEventListener('load', function() {
+
+//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////THEME PICKER//////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+    // Get all the theme buttons
+    var themeButtons = document.querySelectorAll('.theme-button');
+
+    // Add click event listener to each theme button
+    themeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var selectedTheme = button.dataset.theme;
+            applyTheme(selectedTheme);
+        });
+    });
+
+    // Function to apply the selected theme
+    function applyTheme(theme) {
+        // Remove existing theme classes
+        document.body.classList.remove('theme1', 'theme2', 'theme3');
+
+        // Add the selected theme class
+        document.body.classList.add(theme);
+    }
+
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////DATE CALCULATIONS//////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
     var progressBar = document.querySelector('.progress');
     var startDate = new Date(new Date().getFullYear() + '-01-01'); // Specify the start date
     var endDate = new Date(new Date().getFullYear() + '-12-31'); // Specify the end date
