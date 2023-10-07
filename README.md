27dcf501210d8a71b209f973a97a1f5296381521
asher gray <asher@themakerspace.co.za>
2023-10-07 11:51:26 +0200

    page visit update

--- README.md
+++ README.md
@@ -6,10 +6,11 @@
 
 - make an animation for when it hits the new year
 - ~get custom font~
-- slow down inital animation and add ease
+- ~slow down inital animation and add ease~
 
 - ~make colour picker sidebar to change theme~
-- fix 69% animation to be styled correctly, and to not exist after 69% (its invisible but can still select it)
+- ~fix 69% animation to be styled correctly, and to not exist after 69% (its invisible but can still select it)~
+- responsive design
 
 
 maybe:
--- script.js
+++ script.js
@@ -43,12 +43,31 @@ function updateProgress() {
     var percentageElement = document.getElementById('percentage'); // Get the #percentage element
     percentageElement.textContent = yearText + ' is ' + progress + '% complete.'; // Set the text content of the element
 
-    // Check if 'progress' is greater than or equal to 69%
-    if (progress == 69) {
+
+      // Check if 'progress' is greater than or equal to 69%
+    if (progress == 100) {
         // Add a class to the animation text to start the animation
         document.querySelector('.animation-text').classList.add('animate');
       }
 }
+
+//////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////DATE CALCULATIONS//////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+
+var counterContainer = document.querySelector(".visit-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
 
 // Call the updateProgress function initially
 updateProgress();
@@ -56,54 +75,5 @@ updateProgress();
 // Update the progress every second
 setInterval(updateProgress, 10);
 
-//var progressBar = document.querySelector('.progress');
-//var startDate = new Date(new Date().getFullYear() + '-01-01'); // Specify the start date
-//var endDate = new Date(new Date().getFullYear() + '-12-31'); // Specify the end date
-//var totalTime = 30; // Total time in seconds
-//
-//function updateProgress() {
-//    var currentTime = new Date();
-//    var elapsedTime = (currentTime - startDate) / 1000; // Convert milliseconds to seconds
-//    var progress = (elapsedTime / totalTime) * 100;
-//    progressBar.style.width = progress + '%';
-//    var yearText = new Date().getFullYear();
-//    var percentageElement = document.getElementById('percentage');
-//    percentageElement.textContent = yearText + ' is ' + progress + '% complete.';
-//
-//    if (progress >= 69) {
-//        clearInterval(progressInterval); // Stop the interval
-//        const animatedText = document.getElementById('nice-text');
-//        let fontSize = 24; // Initial font size
-//        let opacity = 0; // Initial opacity
-//        const targetFontSize = 48; // Target font size
-//        const targetOpacity = 1; // Target opacity
-//        const duration = 10000; // Animation duration in milliseconds
-//
-//        function animateText() {
-//            if (fontSize < targetFontSize) {
-//                fontSize += 1; // Increase font size
-//                animatedText.style.fontSize = fontSize + 'px';
-//                animatedText.style.transform = 'translate(-50%, -50%) scale(' + (fontSize / targetFontSize) + ')';
-//            }
-//
-//            if (opacity < targetOpacity) {
-//                opacity += 0.02; // Increase opacity
-//                animatedText.style.opacity = opacity;
-//            }
-//
-//            if (fontSize < targetFontSize || opacity < targetOpacity) {
-//                requestAnimationFrame(animateText);
-//            }
-//        }
-//
-//        // Start the animation
-//        animateText();
-//    }
-//}
-//
-//// Call the updateProgress function initially
-//updateProgress();
-//
-//// Update the progress every 300 milliseconds
-//var progressInterval = setInterval(updateProgress, 300);
+
 });
